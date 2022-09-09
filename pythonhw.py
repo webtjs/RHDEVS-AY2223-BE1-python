9f34bb2cef7e0d964ab1888843cc12d133446851
vinatsx <e0775349@u.nus.edu>
2022-09-10 02:38:30 +0800

    new commit

---
+++ pythonhw.py
@@ -0,0 +1,51 @@
+# This program simualtes the backend of a ticket purchasing system
+
+# Price per visitor is $5
+# Price per member is $3.50
+
+# You are to do the following
+# 1. Identify all banned visitors with a filter call
+# 2. Determine the memberships status of all applicants
+# 3. Calculate the total price for all eligible visitors
+# 4. For each valid visitor, return a corresponding ticket in Dictionary form
+# 5. Return an error via thrown exception if applicants is empty
+# Complete everything above in a function called processRequest
+# Your should abstract out function as much as reasonably possible
+
+bannedVisitors = ["Charles", "Grace", "Bruce"]
+memberStatus = {
+    "Ally": True,
+    "David": True,
+    "Brendan": False
+}
+
+request = {
+    "applicants": ["Amy", "Ally", "David", "Brendan", "Zoho"]
+}
+
+
+def processRequest(request):
+    # Your code here
+    return
+
+
+print(processRequest(request))
+
+# {
+#   successfulApplicants:
+#   bannedApplicatns:
+#   totalCost:
+#   tickets: [
+#       {
+#            "name": ,
+#            "membershipStatus": ,
+#            "price":
+#       }, ....
+#   ]
+#
+# }
+
+
+# OR
+
+# {"error": "No applicants"}
